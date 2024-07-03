985edb77a9bd90221f18f294abb1c7b7e85c93fa
ismailgraphix <ismailgrapix@gmail.com>
2024-07-03 19:58:19 +0100

    NOT SURE OF IT

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,10 +20,10 @@ const Navbar = async () =>{
     })
     return(
         <div className="border-b">
-            <div className="flex h-16 items-center px-4">
+            <div className="flex h-16 py-5 items-center px-6">
                 <StoreSwitcher items={stores}/>
                 <MainNav/>
-                <div className="ml-auto flex items-center space-x-4">
+                <div className="ml-auto flex items-center space-x-6">
                     <UserButton afterSignOutUrl="/"/>
                 </div>
                 </div>
